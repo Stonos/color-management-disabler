198ebdc7c5107c78c443f01373db5e124d56bbee
Stonos <stonos@gmail.com>
2019-04-09 22:57:11 +0300

    Add MinHook and GetICMProfileW override

--- version/version.cpp
+++ version/version.cpp
@@ -1,22 +1,69 @@
 #include <windows.h>
 #include <stdio.h>
 #include <tchar.h>
+#include "MinHook.h"
+
+#if defined _M_X64
+#pragma comment(lib, "libMinHook-x64-v141-md.lib")
+#elif defined _M_IX86
+#pragma comment(lib, "libMinHook-x86-v141-md.lib")
+#endif
+
 HINSTANCE mHinst = 0, mHinstDLL = 0;
-extern "C" UINT_PTR mProcs[17] = {0};
+extern "C" UINT_PTR mProcs[17] = { 0 };
 
 void LoadOriginalDll();
+void InitMinHook();
+void UninitMinHook();
 
-LPCSTR mImportNames[] = {"GetFileVersionInfoA", "GetFileVersionInfoByHandle", "GetFileVersionInfoExA", "GetFileVersionInfoExW", "GetFileVersionInfoSizeA", "GetFileVersionInfoSizeExA", "GetFileVersionInfoSizeExW", "GetFileVersionInfoSizeW", "GetFileVersionInfoW", "VerFindFileA", "VerFindFileW", "VerInstallFileA", "VerInstallFileW", "VerLanguageNameA", "VerLanguageNameW", "VerQueryValueA", "VerQueryValueW"};
-BOOL WINAPI DllMain( HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved ) {
+LPCSTR mImportNames[] = { "GetFileVersionInfoA", "GetFileVersionInfoByHandle", "GetFileVersionInfoExA", "GetFileVersionInfoExW", "GetFileVersionInfoSizeA", "GetFileVersionInfoSizeExA", "GetFileVersionInfoSizeExW", "GetFileVersionInfoSizeW", "GetFileVersionInfoW", "VerFindFileA", "VerFindFileW", "VerInstallFileA", "VerInstallFileW", "VerLanguageNameA", "VerLanguageNameW", "VerQueryValueA", "VerQueryValueW" };
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
 	mHinst = hinstDLL;
-	if ( fdwReason == DLL_PROCESS_ATTACH ) {
+	if (fdwReason == DLL_PROCESS_ATTACH) {
 		LoadOriginalDll();
-		for ( int i = 0; i < 17; i++ )
-			mProcs[ i ] = (UINT_PTR)GetProcAddress( mHinstDLL, mImportNames[ i ] );
-	} else if ( fdwReason == DLL_PROCESS_DETACH ) {
-		FreeLibrary( mHinstDLL );
+		for (int i = 0; i < 17; i++) {
+			mProcs[i] = (UINT_PTR)GetProcAddress(mHinstDLL, mImportNames[i]);
+		}
+
+		InitMinHook();
 	}
-	return ( TRUE );
+	else if (fdwReason == DLL_PROCESS_DETACH) {
+		FreeLibrary(mHinstDLL);
+		UninitMinHook();
+	}
+	return (TRUE);
+}
+
+BOOL WINAPI DetourGetICMProfileW(HDC hdc, LPDWORD pBufSize, LPWSTR szFilename) {
+	// we return false so that the monitor's color profile won't be used
+	return false;
+}
+
+void InitMinHook() {
+	// Initialize MinHook.
+	if (MH_Initialize() != MH_OK) {
+		MessageBox(NULL, _T("Unable to init MinHook"), _T(""), NULL);
+		return;
+	}
+
+	// Create a hook for GetICMProfileW, in disabled state.
+	if (MH_CreateHook(&GetICMProfileW, &DetourGetICMProfileW, NULL) != MH_OK) {
+		MessageBox(NULL, _T("Unable to hook GetICMProfileW"), _T(""), NULL);
+		return;
+	}
+
+	// Enable the hook for GetICMProfileW.
+	if (MH_EnableHook(&GetICMProfileW) != MH_OK) {
+		MessageBox(NULL, _T("Unable to enable the GetICMProfileW hook"), _T(""), NULL);
+		return;
+	}
+}
+
+void UninitMinHook() {
+	// Uninitialize MinHook.
+	if (MH_Uninitialize() != MH_OK) {
+		return;
+	}
 }
 
 extern "C" void GetFileVersionInfoA_wrapper();
@@ -37,11 +84,9 @@ extern "C" void VerLanguageNameW_wrapper();
 extern "C" void VerQueryValueA_wrapper();
 extern "C" void VerQueryValueW_wrapper();
 
-
 // Loads the original DLL from the default system directory
-//	Function originally written by Michael Koch
-void LoadOriginalDll()
-{
+// Function originally written by Michael Koch
+void LoadOriginalDll() {
 	char buffer[MAX_PATH];
 
 	// Get path to system dir and to version.dll
@@ -54,8 +99,7 @@ void LoadOriginalDll()
 	if (!mHinstDLL) mHinstDLL = LoadLibrary(buffer);
 
 	// Debug
-	if (!mHinstDLL)
-	{
+	if (!mHinstDLL) {
 		OutputDebugString("PROXYDLL: Original version.dll not loaded ERROR ****\r\n");
 		ExitProcess(0); // Exit the hard way
 	}
